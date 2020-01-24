f0a578a229f4267d37ddfa19598347c9fd1f550a
Ryszka85 <adrian.ryszka@gmx.net>
2020-01-24 19:14:41 +0100

    testProject with DB

--- sample/dbUtil/AddressDbService.java
+++ sample/dbUtil/AddressDbService.java
@@ -13,7 +13,6 @@ public class AddressDbService {
 
     static {
         conn = MysqlConnect.getInstance().connect();
-
         try {
             getUserAddress = conn.prepareStatement(GET_USER_ADDRESS);
         } catch (SQLException e) {
